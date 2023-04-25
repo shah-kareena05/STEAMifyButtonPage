2beed0df77dd93ed2fed7b0e17ce7a3201c54463
Kareena <shah.kareena05@gmail.com>
2023-04-25 12:20:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/steamifybuttonpage/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.steamifybuttonpage;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.steamifybuttonpage.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+
+    public void browser1(View view)
+    {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://airtable.com/shrBN5ShYPSnjrH5B"));
+        startActivity(browserIntent);
+    }
+}
